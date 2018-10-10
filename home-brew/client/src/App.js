e3e1668f4995295c87aaf2debf9743733b0ba20f
Peter K <peterkohler95@gmail.com>
2018-10-10 10:54:50 -0500

    Added beer API

--- home-brew/client/src/App.js
+++ home-brew/client/src/App.js
@@ -6,6 +6,7 @@ import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 import addBeer from "./pages/addBeer/addBeer";
+import Profile from "./pages/Profile/profile";
 
 const App = () => (
   <Router>
@@ -14,6 +15,7 @@ const App = () => (
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/profile" component={Profile} />
         <Route exact path="/addBeer" component={addBeer} />
         <Route component={NoMatch} />
       </Switch>
