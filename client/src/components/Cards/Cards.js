41bd97143148d9f92816b656654670c7c3572f41
Luke Kennin <lkmeq@me.com>
2018-10-14 15:03:10 -0500

    background/fontcolor

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -16,7 +16,7 @@ class Cards extends React.Component {
                     <Col md="4" className="flex-wrap" body>
                         <div style={{ margin: '30px' }}>
                             <a href="/beerInfo"><CardImg width="100%" className="rounded" src="https://www.gannett-cdn.com/-mm-/89934f7b13e7717eb560f3babda84f20895abcd0/c=83-0-724-482/local/-/media/2018/07/17/DetroitFreeP/DetroitFreePress/636674313628993565-GettyImages-684133728.jpg?width=534&height=401&fit=crop" alt="beer" /></a>
-                            <CardBody>
+                            <CardBody style={{ color: 'white' }}>
                                 <CardTitle>Card title</CardTitle>
                                 <CardSubtitle>Card subtitle</CardSubtitle>
                                 <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
@@ -26,7 +26,7 @@ class Cards extends React.Component {
                     <Col md="4" className="flex-wrap" body>
                         <div style={{ margin: '30px' }}>
                             <CardImg width="100%" className="rounded" src="https://www.maxim.com/.image/t_share/MTQ2ODUxNjY1ODIyMDk4OTgw/two-pints-beer-main.jpg" alt="Card image cap" />
-                            <CardBody>
+                            <CardBody style={{ color: 'white' }}>
                                 <CardTitle>Card title</CardTitle>
                                 <CardSubtitle>Card subtitle</CardSubtitle>
                                 <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
@@ -36,7 +36,7 @@ class Cards extends React.Component {
                     <Col md="4" className="flex-wrap" body>
                         <div style={{ margin: '30px' }}>
                             <CardImg top width="100%" className="rounded" src="http://mediad.publicbroadcasting.net/p/wamc/files/201603/beer.jpg" alt="Card image cap" />
-                            <CardBody>
+                            <CardBody style={{ color: 'white' }}>
                                 <CardTitle>Card title</CardTitle>
                                 <CardSubtitle>Card subtitle</CardSubtitle>
                                 <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
@@ -46,7 +46,7 @@ class Cards extends React.Component {
                     <Col md="4" className="flex-wrap" body>
                         <div style={{ margin: '30px' }}>
                             <CardImg top width="100%" className="rounded" src="https://www.mrbeer.com/blog/wp-content/uploads/2017/04/what-is-session-beer.jpg" alt="Card image cap" />
-                            <CardBody>
+                            <CardBody style={{ color: 'white' }}>
                                 <CardTitle>Card title</CardTitle>
                                 <CardSubtitle>Card subtitle</CardSubtitle>
                                 <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
@@ -57,7 +57,7 @@ class Cards extends React.Component {
                     <Col md="4" className="flex-wrap" body>
                         <div style={{ margin: '30px' }}>
                             <CardImg top width="100%" className="rounded" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                            <CardBody>
+                            <CardBody style={{ color: 'white' }}>
                                 <CardTitle>Card title</CardTitle>
                                 <CardSubtitle>Card subtitle</CardSubtitle>
                                 <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
@@ -67,7 +67,7 @@ class Cards extends React.Component {
                     <Col md="4" className="flex-wrap" body>
                         <div style={{ margin: '30px' }}>
                             <CardImg top width="100%" className="rounded" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                            <CardBody>
+                            <CardBody style={{ color: 'white' }}>
                                 <CardTitle>Card title</CardTitle>
                                 <CardSubtitle>Card subtitle</CardSubtitle>
                                 <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
@@ -78,7 +78,7 @@ class Cards extends React.Component {
                     <Col md="4" className="flex-wrap" body>
                         <div style={{ margin: '30px' }}>
                             <CardImg top width="100%" className="rounded" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                            <CardBody>
+                            <CardBody style={{ color: 'white' }}>
                                 <CardTitle>Card title</CardTitle>
                                 <CardSubtitle>Card subtitle</CardSubtitle>
                                 <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
@@ -88,7 +88,7 @@ class Cards extends React.Component {
                     <Col md="4" className="flex-wrap" body>
                         <div style={{ margin: '30px' }}>
                             <CardImg top width="100%" className="rounded" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                            <CardBody>
+                            <CardBody style={{ color: 'white' }}>
                                 <CardTitle>Card title</CardTitle>
                                 <CardSubtitle>Card subtitle</CardSubtitle>
                                 <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
