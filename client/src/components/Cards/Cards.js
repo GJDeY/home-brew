3735ae2dc171b7cf0158ea9bc84e79b71546f853
JorgeBH9 <jorgebh9@outlook.com>
2018-10-15 14:27:31 -0500

    This is the final code working

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -1,35 +1,41 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, CardDeck, Button, Col, Row } from 'reactstrap';
+import API from "../../utils/API";
+
 
 
 class Cards extends React.Component {
 
+
+
     renderCards = () => {
         return this.props.beers.map(beer => {
-            return (
+            return (<div className="container-fluid" style={{ margin: "40px 0px" }}>
+                <Row>
+                    <Col md="4" className="flex-wrap" body>
+                        <div style={{ margin: '30px' }}>
 
-                <div className="col-3">
-                    <Col>
-                        <a href="/beerInfo"><CardImg width="100%" className="rounded" src={beer.imageURL} alt="beer" /></a>
-                        <CardBody style={{ color: "white" }}>
-                            <CardTitle>{beer.beerName} by {beer.brewer}</CardTitle>
-                            <CardSubtitle>{beer.style}</CardSubtitle>
-                            <CardText>{beer.beerNote}</CardText>
-                        </CardBody>
+                            <a href="/beerInfo"><CardImg width="100%" className="rounded" src={beer.imageURL} alt="beer" /></a>
+                            <CardBody style={{ color: "white" }}>
+                                <CardTitle>{beer.beerName}</CardTitle>
+                                <CardSubtitle>{beer.style}</CardSubtitle>
+                                <CardText>{beer.beerNote}</CardText>
+                                <CardText>{beer.hbcAddress}</CardText>
+                                <Button color="dark" body style={{ margin: "10px 10px 10px 10px" }}>Update Beer!</Button>
+                                <Button onClick={() => this.props.handleDeleteBeer(beer._id)} color="dark" body style={{ margin: "10px 10px 10px 10px" }}>Delete Beer!</Button>
+                            </CardBody>
+                        </div>
                     </Col>
-                </div>
 
-
-
+                </Row>
+            </div>
             )
         })
     }
     render() {
         return (
-            <div className="container-fluid">
-                <Row>
-                    {this.renderCards()}
-                </Row>
+            <div>
+                {this.renderCards()}
             </div >
         )
 
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -22,7 +22,9 @@ export default {
   },
 
   getHBC: hbcAddress => {
-    return axios.get("https://api-ropsten.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xCF86a1CFD63E26960A61B7F5Cbc521A189E2522f&address=" + hbcAddress + "&tag=latest&apikey=YourApiKeyToken")
+    // let url = "https://api-ropsten.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xcf86a1cfd63e26960a61b7f5cbc521a189e2522f&address=" + hbcAddress + "&tag=latest&apikey=QCNME6B3SF5IJQMR4RA3S6NQ1UPI1AF66T"
+    let url = 'https://ballup-turned-hoopsgram-api.herokuapp.com/api/plus/courts'
+    return axios.get(url)
   }
 
 };
